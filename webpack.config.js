c99c25a216f4805ccec05dcea81cb0fdfea2b2c3
Rob Lopez <rob.lopez@gmail.com>
2020-10-24 11:23:08 -0700

    Added in JSX support for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,12 +14,12 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       }
     ]
   }
-  
+
 
 };

f21aa50af1148b59756bf3c4c4d4fc06c9e7dbf5
Rob Lopez <rob.lopez@gmail.com>
2020-10-24 11:40:35 -0700

    Working React webpack
    Changed name of root file to app.jsx
    Added script tag to index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.jsx',
+  entry: './src/app.jsx',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
